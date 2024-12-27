6d58935162771e0a586fcc9a07fbb9ca9f292f15
David <157435386+d4vid-vl@users.noreply.github.com>
2024-12-27 20:37:41 -0300

    Added hidden links when button is pressed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,30 @@
+import { useState } from 'react'
 import '../styles/Navbar.css'
 import pfp from '../assets/pfp.png'
 import { Link } from 'react-router-dom'
 import ReorderIcon from '@mui/icons-material/Reorder'
 
 export const Navbar = () => {
+
+  const [showLinks, setShowLinks] = useState(false)
+  const toggleLinks = () => {
+    setShowLinks(!showLinks)
+  }
+
  return (
   <header className = "header">
-    <div>
-      <img src={pfp} alt="Profile"/>
-      <a href="/" className = "logo">pauzz.xyz</a>
+    <div id={showLinks ? "show" : "closed"} className = "main">
+      <div className="leftside">
+        <img src={pfp} alt="Profile" className="pfp"/>
+        <a href="/" className = "logo">pauzz.xyz</a>
+        <div className="hiddenLinks">
+          <Link to="/" className="bar">Home</Link>
+          <Link to="/about" className="bar">About me</Link>
+          <Link to="/portfolio" className="bar">Portfolio</Link>
+          <Link to="/socials" className="bar">Socials</Link>
+          <Link to="/donate" className="bar">Donate me!</Link>
+        </div>
+      </div>
     </div>
     <nav className="navbar">
       <Link to="/" className="bar">Home</Link>
@@ -16,9 +32,9 @@ export const Navbar = () => {
       <Link to="/portfolio" className="bar">Portfolio</Link>
       <Link to="/socials" className="bar">Socials</Link>
       <Link to="/donate" className="bar">Donate me!</Link>
-      <button>
-        <ReorderIcon/>
-      </button>
+    <button onClick={toggleLinks}>
+      <ReorderIcon/>
+    </button>
     </nav>
   </header>
  ) 
