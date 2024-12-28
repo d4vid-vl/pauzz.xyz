e6fefcb9bb0bf41e5f6cf27ea256042748abef00
David <157435386+d4vid-vl@users.noreply.github.com>
2024-12-28 00:16:40 -0300

    List of projects

---
+++ src/helpers/Projects.tsx
@@ -0,0 +1,43 @@
+import pauzz from '../assets/pauzz.png'
+import pangui from '../assets/pangui.png'
+import chan from '../assets/chan.png'
+import doom from '../assets/wip.png'
+import sdltest from '../assets/sdltest.png'
+
+export const Projects = [
+  {
+    name: "ChanLoader",
+    image: chan,
+    language: "Golang",
+    desc: "CLI Tool to download threads from 4Chan",
+    link: "https://github.com/d4vid-vl/ChanLoader"
+  },
+  {
+    name: "Pangui",
+    image: pangui,
+    language: "Rust and Serenity",
+    desc: "Discord bot with my university's useful functionality",
+    link: "https://github.com/d4vid-vl/Pangui"
+  },
+  {
+    name: "pauzz.xyz",
+    image: pauzz,
+    language: "Typescript/React",
+    desc: "Personal page you are in right now!",
+    link: "https://github.com/d4vid-vl/pauzz.xyz"
+  },
+  {
+    name: "DoomClone",
+    image: doom,
+    language: "C",
+    desc: "As the name says, a Doom Clone to learn C (W.I.P)",
+    link: "https://github.com/d4vid-vl/DoomClone"
+  },
+  {
+    name: "SDL2Tests",
+    image: sdltest,
+    language: "C++ and SDL",
+    desc: "Tests for SDL2, drawing stuff on screen and figures!",
+    link: "https://github.com/d4vid-vl/SDL2Tests"
+  }
+]
