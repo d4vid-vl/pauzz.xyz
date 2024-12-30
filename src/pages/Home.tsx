1561e6caad466875050f90ca6110cec9ad636c40
David <157435386+d4vid-vl@users.noreply.github.com>
2024-12-30 20:45:43 -0300

    Temporary home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,12 @@
-import "../styles/Home.css"
+import '../styles/Home.css'
 
 function Home() {
   return (
-    <div>
-      <h2>Hello :3</h2>
+    <div className="homePage">
+      <div className="headerHome">
+       <h1>Hey! I'm David 👋</h1>
+       <p>Im a Software Engineer student in the Pontificia Universidad Católica de Chile</p>
+      </div>
     </div>
   )
 }
