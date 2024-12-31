6d990171365cb013d013fbdacd347a84f2b85872
David <157435386+d4vid-vl@users.noreply.github.com>
2024-12-30 23:22:03 -0300

    Added socials list

---
+++ src/helpers/Social.tsx
@@ -0,0 +1,30 @@
+export const Social = [
+  {
+    name: "Twitter",
+    link: "https://twitter.com/pauzedemrd"
+  },
+  {
+    name: "Github",
+    link: "https://github.com/d4vid-vl"
+  },
+  {
+    name: "Pronouns",
+    link: "https://en.pronouns.page/@pauze"
+  },
+  {
+    name: "RateYourMusic",
+    link: "https://rateyourmusic.com/~pauz"
+  },
+  {
+    name: "Last.fm",
+    link: "https://last.fm/pauzeql"
+  },
+  {
+    name: "Bandcamp 1",
+    link: "https://z610540.bandcamp.com"
+  },
+  {
+    name: "Bandcamp 2",
+    link: "https://seilfer.bandcamp.com"
+  }
+]
