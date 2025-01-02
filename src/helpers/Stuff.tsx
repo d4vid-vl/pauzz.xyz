a76852b1a20ddeb40358a9fed3d1b3ba759585b0
David <157435386+d4vid-vl@users.noreply.github.com>
2025-01-02 02:50:21 -0300

    Added "experience" in programming and more stuff

---
+++ src/helpers/Stuff.tsx
@@ -0,0 +1,90 @@
+import CIcon from "../assets/langstuff/clang.svg";
+import CppIcon from "../assets/langstuff/cpp.svg";
+import PythonIcon from "../assets/langstuff/python.svg";
+import GoIcon from "../assets/langstuff/golang.svg";
+import JuliaIcon from "../assets/langstuff/julia.svg";
+import RustIcon from "../assets/langstuff/rust.svg";
+
+import TypeScriptIcon from "../assets/langstuff/typescript.svg";
+import ReactIcon from "../assets/langstuff/react.svg";
+
+import NeoVimIcon from "../assets/langstuff/neovim.svg";
+import CMakeIcon from "../assets/langstuff/cmake.svg";
+import SDLIcon from "../assets/langstuff/sdl.svg";
+
+import AbletonIcon from "../assets/langstuff/ableton.svg";
+import PhotoshopIcon from "../assets/langstuff/photoshop.svg";
+import PremiereIcon from "../assets/langstuff/premiere.svg";
+import ManimIcon from "../assets/langstuff/manim.svg";
+
+export const BackEnd = [
+  {
+    name: "C",
+    svg: CIcon 
+  },
+  {
+    name: "Cpp",
+    svg: CppIcon
+  },
+  {
+    name: "Python",
+    svg: PythonIcon
+  },
+  {
+    name: "Go",
+    svg: GoIcon
+  },
+  {
+    name: "Julia",
+    svg: JuliaIcon
+  },
+  {
+    name: "Rust",
+    svg: RustIcon
+  }
+]
+
+export const FrontEnd = [
+  {
+    name: "TypeScript",
+    svg: TypeScriptIcon
+  },
+  {
+    name: "React",
+    svg: ReactIcon
+  }
+]
+
+export const DevTools = [
+  {
+    name: "NeoVim",
+    svg: NeoVimIcon
+  },
+  {
+    name: "CMake",
+    svg: CMakeIcon
+  },
+  {
+    name: "SDL",
+    svg: SDLIcon
+  },
+]
+
+export const CreativeTools = [
+  {
+    name: "Ableton",
+    svg: AbletonIcon
+  },
+  {
+    name: "Photoshop",
+    svg: PhotoshopIcon
+  },
+  {
+    name: "Premiere",
+    svg: PremiereIcon
+  },
+  {
+    name: "Manim",
+    svg: ManimIcon
+  },
+]
