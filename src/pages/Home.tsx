137237178f5fbeedac8d598258dee86a309741d6
David <157435386+d4vid-vl@users.noreply.github.com>
2025-01-03 01:39:09 -0300

    Added maxwell in the bottom of every page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,15 @@
 import '../styles/Home.css'
 
+import Maxwell from '../assets/About/maxwell.gif'
+
 function Home() {
   return (
     <div className="homePage">
       <div className="headerHome">
-       <h1>Hey! I'm David 👋</h1>
-       <p>Im a Software Engineer student in the Pontificia Universidad Católica de Chile</p>
-      </div>
+        <h1>Hey! I'm David 👋</h1>
+        <p>Im a Software Engineer student in the Pontificia Universidad Católica de Chile</p>
+        <img src={Maxwell}/>
+       </div>
     </div>
   )
 }
--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,20 +1,22 @@
 import { Project } from '../components/Project'
 import { Projects } from '../helpers/Projects'
+import Maxwell from '../assets/About/maxwell.gif'
 import '../styles/Portfolio.css'
 
 function Portfolio() {
   return (
     <div className="portfolioPage">
-      <h2 className="portfolioTitle">Currently I'm learning a lot of languages and how to code</h2>
+      <h1 className="portfolioTitle">Currently I'm learning a lot of languages and how to code</h1>
       <h3>Here are some of my projects I've been working on!</h3>
       <div className="projectsList">
         {Projects.map((item,key) => {
           return (
             <Project key={key} name={item.name} image={item.image} language={item.language} desc={item.desc} link={item.link}/>
           )
-        })} 
+        })}
       </div>
       <p>TODO: Add better images and maybe videos</p>
+      <img src={Maxwell}/> 
     </div>
   )
 }
